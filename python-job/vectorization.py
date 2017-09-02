321081c577ee33f774866f47d8985a54739856cb
Eric <eric.appelt@gmail.com>
2017-09-02 14:58:52 -0500

    Migrate python example to anaconda3
    Use anaconda3 in python example script, and update the
    script to use up to date python3 features including
    f-strings.
    
    Change time.clock to time.process_time as the former
    is deprecated as of python 3.3.
    
    See RT 37626 for motivation.

--- python-job/vectorization.py
+++ python-job/vectorization.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
-#
-# Python 2.7 script demonstrating vectorized execution
-#
+"""
+Python 3.6 script demonstrating vectorized execution
+"""
 import numpy as np
 import time
+
 
 # 10 million entries
 t = np.linspace(-10,10,10000000)
 x1 = np.zeros(len(t))
 x2 = np.zeros(len(t))
 
-time1 = time.clock()
+time1 = time.process_time()
 # naive, non-vectorized implementation
-for i,ti in enumerate(t):
-	x1[i] = np.sin(ti)
-time2 = time.clock()
-print '%s: %0.2f seconds elapsed' % ("naive implementation", time2-time1)
+for i, ti in enumerate(t):
+    x1[i] = np.sin(ti)
+time2 = time.process_time()
+print(f"naive implementation {time2-time1:.2f} cpu seconds elapsed")
 
 # vectorized implementation
-time1 = time.clock()
+time1 = time.process_time()
 x2 = np.sin(t)
-time2 = time.clock()
-print '%s: %0.2f seconds elapsed' % ("vectorized implementation", time2-time1)
+time2 = time.process_time()
+print(f"vectorized implementation {time2-time1:.2f} cpu seconds elapsed")
 
-if ( np.array_equal(x1,x2) ):
-	print "arrays equal!"
+if np.array_equal(x1,x2):
+	print("arrays equal!")
