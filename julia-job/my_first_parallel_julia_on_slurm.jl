92c6a3b2d6594420c55f9684829ea69f6d93ded6
Nick Eubank <nickeubank@gmail.com>
2018-01-05 12:40:37 -0600

    refine based on issue discussed in RT 41483

--- julia-job/my_first_parallel_julia_on_slurm.jl
+++ julia-job/my_first_parallel_julia_on_slurm.jl
@@ -3,15 +3,18 @@
 # can be installed with the command
 # `Pkg.add("ClusterManagers")`
 
-# Recall we asked for 4 processes in the `.slurm` file.
+# Recall we asked for 5 processes in the `.slurm` file.
+# This managing process is one of those processes, so
+# now we can spin off 4 more.
 # These are created using either `addprocs_slurm(4)` or
 # `addprocs(SlurmManager(4))`.
 
-# This will create worker instances that work the same way as
+# This will create worker instances that work the same way
+# (from your perspective) as
 # the workers that would be created if you'd launched julia
 # on your own computer with `julia -p 4` or as if you'd
 # openned Julia and run
-# `addprocs(4)`.
+# `addprocs(4)`
 
 using ClusterManagers
 
